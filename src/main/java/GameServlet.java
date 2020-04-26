1d33d9715dc91157ed132b063b2d6cdf0278614b
Andrey <kouznts@ya.ru>
2020-04-26 21:33:03 +0400

    получение и отправка размеров

--- src/main/java/GameServlet.java
+++ src/main/java/GameServlet.java
@@ -10,6 +10,14 @@ public class GameServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        String [] sizes = request.getParameterValues("sizes");
+
+        int horizontal = Integer.parseInt(sizes[0]);
+        int vertical = Integer.parseInt(sizes[1]);
+
+        request.setAttribute("horizontal", horizontal);
+        request.setAttribute("vertical", vertical);
+
         getServletContext().
                 getRequestDispatcher("/game.jsp").forward(request, response);
     }
